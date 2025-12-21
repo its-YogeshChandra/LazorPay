a9db02c5b092ab822ef59e0b19b8574d8a4b9255
its-YogeshChandra <pandittheroyal@gmail.com>
2025-12-21 21:30:47 +0530

    adding lazorkit sdk

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,22 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Button } from "react-native"
+import { LazorKitProvider, useWallet } from '@lazorkit/wallet-mobile-adapter'
+
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-blue-600">
+    <LazorKitProvider
+      rpcUrl="https://api.devnet.solana.com"
+      portalUrl="https://portal.lazor.sh"
+      configPaymaster={{
+        paymasterUrl: "https://kora.devnet.lazorkit.com"
+      }}>
+      <View className="flex-1 bg-blue-600">
+        <Button
+          title="Uranus"
+        />
+      </View>
 
-    </View>
+    </LazorKitProvider >
   )
 }
